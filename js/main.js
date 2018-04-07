c0a90d1f1a7937ce0383de661f3b7b3221b9e912
Minami Kato <minamikato@gmail.com>
2018-04-07 15:32:23 -0400

    added eventlistener

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+let price = document.getElementById('price');
+let num = document.getElementById('num');
+let unit = document.getElementById('unit');
+let btn = document.getElementById('btn');
+let result = document.getElementById('result');
+let reset = document.getElementById('reset');
+
+  btn.addEventListener('click', function(){
+
+  });
+})();
